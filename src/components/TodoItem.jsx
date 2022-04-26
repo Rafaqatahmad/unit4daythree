c3d583954dbf4749fda586952a5aefe2626fb40d
Rafaqat Ahmad <irahilabassi@gmail.com>
2022-04-27 00:48:16 +0530

    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,10 @@
+
+import "./TodoItem.css";
+export const TodoItem = ({ todo, completed }) => {
+  return (
+    <div className="todolist">
+      <button onClick={() => completed(todo.id)}>✔️</button>
+      {todo.title}
+    </div>
+  );
+};
